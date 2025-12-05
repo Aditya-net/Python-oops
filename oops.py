402c7dcc62eb932dcdc1a0d33e7e3be9edc8bf45
Aditya-net <adityawagh1348@gmail.com>
2025-12-05 12:24:33 +0530

    Attributes and classes

---
+++ oops.py
@@ -0,0 +1,8 @@
+class Aditya:
+    a=111 #attribute
+    def hello(self):
+        print("Hello, Aditya here!") #method  
+
+    print("This is Aditya's class")  
+print(Aditya().a)   
+Aditya().hello()
